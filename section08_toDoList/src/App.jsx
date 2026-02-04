08f4adec198c65b7abf32140eae462c592987747
SUNHO <sh525293@gmail.com>
2026-02-05 04:06:05 +0900

    Add sec08 - ToDoList Create

---
+++ section08_toDoList/src/App.jsx
@@ -0,0 +1,55 @@
+import "./App.css";
+import { useState, useRef } from "react";
+import Header from "./components/Header";
+import Editor from "./components/Editor";
+import List from "./components/List";
+
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    date: new Date().getTime,
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "빨래하기",
+    date: new Date().getTime,
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "춤 연습하기",
+    date: new Date().getTime,
+  },
+];
+
+function App() {
+  const [todos, setTodos] = useState(mockData);
+  const idRef = useRef(3);
+
+  // Editor로부터 받은 값을 통해 새로운 객체 생성
+  // 해당 객체를 todos 배열에 넣으며 리렌더링
+  // 이 때, todos.push(newToDO) 이렇게 하면 값만 넣을 뿐 리렌더링 x
+  const onCreate = (content) => {
+    const newToDo = {
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+      date: new Date().getTime(),
+    };
+
+    setTodos([newToDo, ...todos]);
+  };
+
+  return (
+    <div className="App">
+      <Header />
+      <Editor onCreate={onCreate} />
+      <List />
+    </div>
+  );
+}
+
+export default App;
